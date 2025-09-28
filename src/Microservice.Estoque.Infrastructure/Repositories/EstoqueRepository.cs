70b890ad509ffa2d0363f886f667833fc3dcc29a
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-09-28 13:29:47 -0300

    Refatora EstoqueRepository para maior clareza e consistência
    - Remove importações redundantes de namespaces.
    - Ajusta o namespace para `Microservice.Estoque.Infrastructure.Repositories`.
    - Usa referência explícita para `Domain.Entities.Estoque`.
    - Atualiza retorno do método `GetByProdutoIdAsync` para refletir a nova referência explícita.

--- src/Microservice.Estoque.Infrastructure/Repositories/EstoqueRepository.cs
+++ src/Microservice.Estoque.Infrastructure/Repositories/EstoqueRepository.cs
@@ -1,15 +1,14 @@
-using Microsoft.EntityFrameworkCore;
 using Microservice.Estoque.Application.Interfaces;
-using Microservice.Estoque.Domain.Entities;
 using Microservice.Estoque.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Microservice.Estoque.Infrastructure.Repositories;
 
-public class EstoqueRepository : Repository<Estoque>, IEstoqueRepository
+public class EstoqueRepository : Repository<Domain.Entities.Estoque>, IEstoqueRepository
 {
     public EstoqueRepository(EstoqueDbContext ctx) : base(ctx) { }
 
-    public async Task<Estoque?> GetByProdutoIdAsync(int produtoId)
+    public async Task<Domain.Entities.Estoque?> GetByProdutoIdAsync(int produtoId)
     {
         return await _ctx.Estoques.FirstOrDefaultAsync(e => e.ProdutoId == produtoId);
     }
